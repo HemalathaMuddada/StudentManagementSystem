aa68165325061423d77a53782ed57a9b9e3dcab5
HemalathaMuddada <hemalathamuddada2@gmail.com>
2021-11-22 23:32:06 +0530

    My first day status

---
+++ StudentManagementSystem/src/main/java/com/project/sms/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.project.sms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.project.sms.entity.*;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	 @Query("SELECT DISTINCT user FROM User user " +
+	            "INNER JOIN FETCH user.authorities AS authorities " +
+	            "WHERE user.username = :username")
+	    User findByUsername(@Param("username") String username);
+
+}
--- StudentManagementSystem/src/main/resources/postgresql/user.sql
+++ StudentManagementSystem/src/main/resources/postgresql/user.sql
@@ -7,11 +7,11 @@ CREATE TABLE IF NOT EXISTS public."user_info"
     password character varying(255) NOT NULL,
     first_name character varying(255) NOT NULL,
     last_name character varying(255) NOT NULL,
-    aadhar_number integer NOT NULL,
+    email character varying(255) NOT NULL,
     CONSTRAINT user_pkey PRIMARY KEY (id),
     CONSTRAINT user_name_unique_index UNIQUE (user_name)
 )
 
-INSERT INTO "user_info"(ID, USER_NAME, PASSWORD,first_name,last_name,aadhar_number)
+INSERT INTO "user_info"(ID, USER_NAME, PASSWORD,first_name,last_name,email)
   VALUES (1, 'superadmin@demo.com', /*admin*/'$2a$08$WhflkbQx9CwspjvWr2gGu.Mc9zGUshO5u70R.b2rCvjc91DS2ABbK', 
-		  'Super', 'Admin', 0000000000);
+		  'Super', 'Admin', 'superadmin@demo.com');
